acce73abd0a877621c1222680566b67c695a5b26
kyle <lelegrau04@gmail.com>
2026-03-21 14:06:02 +0100

    feat: add ServerConfig seeding for deployment
    setServerConfig reducer + seed script for injecting auth secrets into WASM runtime.

--- spacetimedb/src/index.ts
+++ spacetimedb/src/index.ts
@@ -4,3 +4,4 @@ export { default } from './schema.js';
 export { connectUser } from './reducers/identity.js';
 export { createCollection, updateCollection, deleteCollection } from './reducers/collections.js';
 export { addToCollection, updateCollectionCard, removeFromCollection } from './reducers/collection-cards.js';
+export { setServerConfig } from './reducers/admin.js';
