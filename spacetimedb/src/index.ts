d517e3e0973cbe39b875d00cf0b9f216af05048e
kyle <lelegrau04@gmail.com>
2026-03-21 13:50:41 +0100

    feat: add collection CRUD reducers
    createCollection, updateCollection, deleteCollection with ownership validation.

--- spacetimedb/src/index.ts
+++ spacetimedb/src/index.ts
@@ -2,3 +2,4 @@ export { default } from './schema.js';
 
 // --- Reducers ---
 export { connectUser } from './reducers/identity.js';
+export { createCollection, updateCollection, deleteCollection } from './reducers/collections.js';
